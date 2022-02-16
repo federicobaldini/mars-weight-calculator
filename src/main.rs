860c165e5d59331269a29744fed7bdaf19e33935
federicobaldini <federico-baldini@hotmail.com>
2022-02-16 18:17:21 +0100

    added function to calculate weight on mars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+  println!("Equivalent weight on Mars: {}kg", calculate_weight_on_mars(100.0));
+}
+
+fn calculate_weight_on_mars(weight_on_earth: f32) -> f32 {
+  (weight_on_earth / 9.81) * 3.711
 }
